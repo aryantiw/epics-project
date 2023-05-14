8ccdf0f2cae5e847aa19f93c48f04880b40add15
aryantiw <aryantiwari543@gmail.com>
2023-05-14 18:58:48 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -21,8 +21,10 @@ def predict_sentiment(preprocessed_text):
         return 'neutral'
     elif prediction == 0:
         return 'positive'
+    elif prediction == -1:
+        return 'negative'
     else:
-        return 'Negative'
+        return 'Neutral'
 
 # Define the Streamlit app
 def main():
